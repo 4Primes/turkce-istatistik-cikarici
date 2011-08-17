18c6c5235f1c09dc8b6ee25e33663afd79c28cae
Ecem <ecem@ecem-VirtualBox.(none)>
2011-08-17 13:32:51 +0300

    ilk 20 kelimenin gösterilmesi

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -24,185 +24,185 @@ import com.google.gson.Gson;
 
 public class Application extends Controller {
 
-    public static final Zemberek z = new Zemberek(new TurkiyeTurkcesi());
+	public static final Zemberek z = new Zemberek(new TurkiyeTurkcesi());
 
-    public static void index() {
+	public static void index() {
 
-        render();
-    }
+		render();
+	}
 
-    public static void kullanicidanAl(@Required String text) {
+	public static void kullanicidanAl(@Required String text) {
 
-        if (validation.hasErrors()) {
-            flash.error("Text girmeniz gerekiyor!");
-            index();
-        }
-        render("Application/kullanicidanAl.html", text);
-    }
+		if (validation.hasErrors()) {
+			flash.error("Text girmeniz gerekiyor!");
+			index();
+		}
+		render("Application/kullanicidanAl.html", text);
+	}
 
-    public static void dosyadanOku(@Required File dosyaadı) {
+	public static void dosyadanOku(@Required File dosyaadı) {
 
-        if (validation.hasErrors()) {
-            flash.error("Dosya upload etmediniz!");
-            index();
-        }
-        String a = "";
-        try {
-            BufferedReader oku = new BufferedReader(new FileReader(dosyaadı));
+		if (validation.hasErrors()) {
+			flash.error("Dosya upload etmediniz!");
+			index();
+		}
+		String a = "";
+		try {
+			BufferedReader oku = new BufferedReader(new FileReader(dosyaadı));
 
-            while (oku.ready()) {
-                a += oku.readLine();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        render("Application/dosyadanOku.html", a);
-    }
+			while (oku.ready()) {
+				a += oku.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		render("Application/dosyadanOku.html", a);
+	}
 
-    public static String duzenle(String k) {
+	public static String duzenle(String k) {
 
-        // k = k.replaceAll("\\W", " ").replaceAll("\\s+", " ");;
-        k = k.replace("...", " ");
-        k = k.replace(". ", " ");
-        k = k.replace("! ", " ");
-        k = k.replace("? ", " ");
-        k = k.replace(", ", " ");
-        k = k.replace("'", " ");
-        k = k.replace("\r", " ");
-        return k;
-    }
+		// k = k.replaceAll("\\W", " ").replaceAll("\\s+", " ");;
+		k = k.replace("...", " ");
+		k = k.replace(". ", " ");
+		k = k.replace("! ", " ");
+		k = k.replace("? ", " ");
+		k = k.replace(", ", " ");
+		k = k.replace("'", " ");
+		k = k.replace("\r", " ");
+		return k;
+	}
 
-    public static void heceler(String k) {
+	public static void heceler(String k) {
 
-        k = duzenle(k);
-        String dizi[] = k.split(" ");
-        List<String[]> hecelenmis = new ArrayList<String[]>();
-        for (String kelime : dizi) {
-            try {
-                if (z.hecele(kelime) != null)
-                    hecelenmis.add(z.hecele(kelime));
-            } catch (Exception e) {
-                Logger.error(e, "bir hata oluştu");
-            }
-        }
-        render("Application/heceler.html", hecelenmis);
-    }
+		k = duzenle(k);
+		String dizi[] = k.split(" ");
+		List<String[]> hecelenmis = new ArrayList<String[]>();
+		for (String kelime : dizi) {
+			try {
+				if (z.hecele(kelime) != null)
+					hecelenmis.add(z.hecele(kelime));
+			} catch (Exception e) {
+				Logger.error(e, "bir hata oluştu");
+			}
+		}
+		render("Application/heceler.html", hecelenmis);
+	}
 
-    public static void kelimeAyristir(String k) {
+	public static void kelimeAyristir(String k) {
 
-        k = duzenle(k);
-        String[] dizi = k.split(" ");
-        List<List<String[]>> ayrisimlar = new ArrayList<List<String[]>>();
-        for (String kelime : dizi) {
-            ayrisimlar.add(z.kelimeAyristir(kelime));
-        }
-        render("Application/kelimeAyristir.html", ayrisimlar);
-    }
+		k = duzenle(k);
+		String[] dizi = k.split(" ");
+		List<List<String[]>> ayrisimlar = new ArrayList<List<String[]>>();
+		for (String kelime : dizi) {
+			ayrisimlar.add(z.kelimeAyristir(kelime));
+		}
+		render("Application/kelimeAyristir.html", ayrisimlar);
+	}
 
-    public static void kelimeCozumle(String k) {
+	public static void kelimeCozumle(String k) {
 
-        k = duzenle(k);
-        String dizi[] = k.split(" ");
-        List<List<Kelime>> gecis = new ArrayList<List<Kelime>>();
-        Kelime[] cozumler = null;
-        for (String kel : dizi) {
-            cozumler = z.kelimeCozumle(kel);
-            List<Kelime> cozum = Arrays.asList(cozumler);
-            gecis.add(cozum);
-        }
-        render("Application/kelimeCozumle.html", gecis);
-    }
+		k = duzenle(k);
+		String dizi[] = k.split(" ");
+		List<List<Kelime>> gecis = new ArrayList<List<Kelime>>();
+		Kelime[] cozumler = null;
+		for (String kel : dizi) {
+			cozumler = z.kelimeCozumle(kel);
+			List<Kelime> cozum = Arrays.asList(cozumler);
+			gecis.add(cozum);
+		}
+		render("Application/kelimeCozumle.html", gecis);
+	}
 
-    public static void asciDonustur(String k) {
+	public static void asciDonustur(String k) {
 
-        String l = z.asciiyeDonustur(k);
-        render("Application/asciDonustur.html", l);
-    }
+		String l = z.asciiyeDonustur(k);
+		render("Application/asciDonustur.html", l);
+	}
 
-    public static void oneriler(String k) {
+	public static void oneriler(String k) {
 
-        k = duzenle(k);
-        String[] dizi = k.split(" ");
-        List<String[]> gecis = new ArrayList<String[]>();
+		k = duzenle(k);
+		String[] dizi = k.split(" ");
+		List<String[]> gecis = new ArrayList<String[]>();
 
-        for (String oneri : dizi) {
-            gecis.add(z.oner(oneri));
-        }
-        render("Application/oneriler.html", gecis);
-    }
+		for (String oneri : dizi) {
+			gecis.add(z.oner(oneri));
+		}
+		render("Application/oneriler.html", gecis);
+	}
 
-    public static void kelimeDenetle(String k) {
+	public static void kelimeDenetle(String k) {
 
-        k = duzenle(k);
-        String dizi[] = null;
-        dizi = k.split(" ");
-        int a = 0;
-        String l = "";
-        String denetle[] = new String[dizi.length];
-        while (a < dizi.length) {
-            if (z.kelimeDenetle(dizi[a]))
-                l = "Kelime doğru yazilmis";
-            else
-                l = "Kelime yanlış yazilmis";
-            denetle[a] = l;
-            a++;
-        }
+		k = duzenle(k);
+		String dizi[] = null;
+		dizi = k.split(" ");
+		int a = 0;
+		String l = "";
+		String denetle[] = new String[dizi.length];
+		while (a < dizi.length) {
+			if (z.kelimeDenetle(dizi[a]))
+				l = "Kelime doğru yazilmis";
+			else
+				l = "Kelime yanlış yazilmis";
+			denetle[a] = l;
+			a++;
+		}
 
-        render("Application/kelimeDenetle.html", denetle);
-    }
+		render("Application/kelimeDenetle.html", denetle);
+	}
 
-    public static void kokBul(String k) {
+	public static void kokBul(String k) {
 
-        k = duzenle(k);
-        String[] dizi = k.split(" ");
-        KokBulucu kok = z.kokBulucu();
-        List<String[]> gecis = new ArrayList<String[]>();
+		k = duzenle(k);
+		String[] dizi = k.split(" ");
+		KokBulucu kok = z.kokBulucu();
+		List<String[]> gecis = new ArrayList<String[]>();
 
-        for (String kokk : dizi) {
-            gecis.add(kok.stringKokBul(kokk));
-        }
-        render("Application/kokBul.html", gecis);
-    }
+		for (String kokk : dizi) {
+			gecis.add(kok.stringKokBul(kokk));
+		}
+		render("Application/kokBul.html", gecis);
+	}
 
-    public static void sayiBul(String k) {
+	public static void sayiBul(String k) {
 
-        k = duzenle(k);
-        String[] dizi = k.split(" ");
-        KokBulucu kok = z.kokBulucu();
-        String[] gecis = null;
-        String[] kokler = null;
-        gecis = new String[dizi.length];
+		k = duzenle(k);
+		String[] dizi = k.split(" ");
+		KokBulucu kok = z.kokBulucu();
+		String[] gecis = null;
+		String[] kokler = null;
+		gecis = new String[dizi.length];
 
-        for (int i = 0; i < dizi.length; i++) {
-            kokler = kok.stringKokBul(dizi[i]);
-            try {
-                if (!"".equals(kokler[0]))
-                    gecis[i] = kokler[0];
-            } catch (Exception e) {
-                Logger.error(e, "bir hata oluştu");
-            }
-        }
-        Map<String, Integer> kelimeSayi = new HashMap<String, Integer>();
-        for (String kelime : gecis) {
+		for (int i = 0; i < dizi.length; i++) {
+			kokler = kok.stringKokBul(dizi[i]);
+			try {
+				if (!"".equals(kokler[0]))
+					gecis[i] = kokler[0];
+			} catch (Exception e) {
+				Logger.error(e, "bir hata oluştu");
+			}
+		}
+		Map<String, Integer> kelimeSayi = new HashMap<String, Integer>();
+		for (String kelime : gecis) {
 
-            if (kelimeSayi.get(kelime) == null) {
-                kelimeSayi.put(kelime, 1);
-            } else {
-                kelimeSayi.put(kelime, kelimeSayi.get(kelime) + 1);
-            }
+			if (kelimeSayi.get(kelime) == null) {
+				kelimeSayi.put(kelime, 1);
+			} else {
+				kelimeSayi.put(kelime, kelimeSayi.get(kelime) + 1);
+			}
 
-        }
+		}
 
-        TreeMap treeMap = new TreeMap(new MyComparator(kelimeSayi));
-        treeMap.putAll(kelimeSayi);
-        kelimeSayi = treeMap;
+		TreeMap treeMap = new TreeMap(new MyComparator(kelimeSayi));
+		treeMap.putAll(kelimeSayi);
+		kelimeSayi = treeMap;
 
-        Set<String> keys = kelimeSayi.keySet();
-        Collection<Integer> values = kelimeSayi.values();
-        String kelimeDizisi = new Gson().toJson(keys);
-        String sayiDizisi = new Gson().toJson(values);
+		Set<String> keys = kelimeSayi.keySet();
+		Collection<Integer> values = kelimeSayi.values();
+		String kelimeDizisi = new Gson().toJson(keys);
+		String sayiDizisi = new Gson().toJson(values);
 
-        render("Application/sayiBul.html", kelimeDizisi, sayiDizisi);
+		render("Application/sayiBul.html", kelimeDizisi, sayiDizisi);
 
-    }
+	}
 }
--- app/controllers/MyComparator.java
+++ app/controllers/MyComparator.java
@@ -5,21 +5,21 @@ import java.util.Map;
 
 public class MyComparator implements Comparator<Object> {
 
-    Map theMapToSort;
+	Map theMapToSort;
 
-    public MyComparator(Map theMapToSort) {
+	public MyComparator(Map theMapToSort) {
 
-        this.theMapToSort = theMapToSort;
-    }
+		this.theMapToSort = theMapToSort;
+	}
 
-    public int compare(Object key1, Object key2) {
+	public int compare(Object key1, Object key2) {
 
-        Integer val1 = (Integer) theMapToSort.get(key1);
-        Integer val2 = (Integer) theMapToSort.get(key2);
-        if (val1 < val2) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
+		Integer val1 = (Integer) theMapToSort.get(key1);
+		Integer val2 = (Integer) theMapToSort.get(key2);
+		if (val1 < val2) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
 }
